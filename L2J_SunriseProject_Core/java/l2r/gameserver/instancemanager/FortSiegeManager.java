e4b97780e61f71564fe8562cf1f2b359d2702454
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2015-02-24 15:12:04 +0000

    Core rev 1

---
+++ L2J_SunriseProject_Core/java/l2r/gameserver/instancemanager/FortSiegeManager.java
@@ -0,0 +1,377 @@
+/*
+ * Copyright (C) 2004-2015 L2J Server
+ * 
+ * This file is part of L2J Server.
+ * 
+ * L2J Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * L2J Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package l2r.gameserver.instancemanager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import javolution.util.FastList;
+import javolution.util.FastMap;
+import l2r.Config;
+import l2r.L2DatabaseFactory;
+import l2r.gameserver.model.CombatFlag;
+import l2r.gameserver.model.FortSiegeSpawn;
+import l2r.gameserver.model.L2Clan;
+import l2r.gameserver.model.L2Object;
+import l2r.gameserver.model.actor.instance.L2PcInstance;
+import l2r.gameserver.model.entity.Fort;
+import l2r.gameserver.model.entity.FortSiege;
+import l2r.gameserver.model.items.instance.L2ItemInstance;
+import l2r.gameserver.model.skills.CommonSkill;
+import l2r.gameserver.network.SystemMessageId;
+import l2r.gameserver.network.serverpackets.SystemMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FortSiegeManager
+{
+	private static final Logger _log = LoggerFactory.getLogger(FortSiegeManager.class);
+	
+	public static final FortSiegeManager getInstance()
+	{
+		return SingletonHolder._instance;
+	}
+	
+	private int _attackerMaxClans = 500; // Max number of clans
+	
+	// Fort Siege settings
+	private FastMap<Integer, FastList<FortSiegeSpawn>> _commanderSpawnList;
+	private FastMap<Integer, FastList<CombatFlag>> _flagList;
+	private boolean _justToTerritory = true; // Changeable in fortsiege.properties
+	private int _flagMaxCount = 1; // Changeable in fortsiege.properties
+	private int _siegeClanMinLevel = 4; // Changeable in fortsiege.properties
+	private int _siegeLength = 60; // Time in minute. Changeable in fortsiege.properties
+	private int _countDownLength = 10; // Time in minute. Changeable in fortsiege.properties
+	private int _suspiciousMerchantRespawnDelay = 180; // Time in minute. Changeable in fortsiege.properties
+	private List<FortSiege> _sieges;
+	
+	protected FortSiegeManager()
+	{
+		load();
+	}
+	
+	public final void addSiegeSkills(L2PcInstance character)
+	{
+		character.addSkill(CommonSkill.SEAL_OF_RULER.getSkill(), false);
+		character.addSkill(CommonSkill.BUILD_HEADQUARTERS.getSkill(), false);
+	}
+	
+	/**
+	 * @param clan The L2Clan of the player
+	 * @param fortid
+	 * @return true if the clan is registered or owner of a fort
+	 */
+	public final boolean checkIsRegistered(L2Clan clan, int fortid)
+	{
+		if (clan == null)
+		{
+			return false;
+		}
+		
+		boolean register = false;
+		try (Connection con = L2DatabaseFactory.getInstance().getConnection();
+			PreparedStatement ps = con.prepareStatement("SELECT clan_id FROM fortsiege_clans where clan_id=? and fort_id=?"))
+		{
+			ps.setInt(1, clan.getId());
+			ps.setInt(2, fortid);
+			try (ResultSet rs = ps.executeQuery())
+			{
+				while (rs.next())
+				{
+					register = true;
+					break;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			_log.warn("Exception: checkIsRegistered(): " + e.getMessage(), e);
+		}
+		return register;
+	}
+	
+	public final void removeSiegeSkills(L2PcInstance character)
+	{
+		character.removeSkill(CommonSkill.SEAL_OF_RULER.getSkill());
+		character.removeSkill(CommonSkill.BUILD_HEADQUARTERS.getSkill());
+	}
+	
+	private final void load()
+	{
+		Properties siegeSettings = new Properties();
+		final File file = new File(Config.FORTSIEGE_CONFIGURATION_FILE);
+		try (InputStream is = new FileInputStream(file))
+		{
+			siegeSettings.load(is);
+		}
+		catch (Exception e)
+		{
+			_log.warn("Error while loading Fort Siege Manager settings!", e);
+		}
+		
+		// Siege setting
+		_justToTerritory = Boolean.parseBoolean(siegeSettings.getProperty("JustToTerritory", "true"));
+		_attackerMaxClans = Integer.decode(siegeSettings.getProperty("AttackerMaxClans", "500"));
+		_flagMaxCount = Integer.decode(siegeSettings.getProperty("MaxFlags", "1"));
+		_siegeClanMinLevel = Integer.decode(siegeSettings.getProperty("SiegeClanMinLevel", "4"));
+		_siegeLength = Integer.decode(siegeSettings.getProperty("SiegeLength", "60"));
+		_countDownLength = Integer.decode(siegeSettings.getProperty("CountDownLength", "10"));
+		_suspiciousMerchantRespawnDelay = Integer.decode(siegeSettings.getProperty("SuspiciousMerchantRespawnDelay", "180"));
+		
+		// Siege spawns settings
+		_commanderSpawnList = new FastMap<>();
+		_flagList = new FastMap<>();
+		
+		for (Fort fort : FortManager.getInstance().getForts())
+		{
+			FastList<FortSiegeSpawn> _commanderSpawns = new FastList<>();
+			FastList<CombatFlag> _flagSpawns = new FastList<>();
+			for (int i = 1; i < 5; i++)
+			{
+				String _spawnParams = siegeSettings.getProperty(fort.getName().replace(" ", "") + "Commander" + i, "");
+				if (_spawnParams.isEmpty())
+				{
+					break;
+				}
+				StringTokenizer st = new StringTokenizer(_spawnParams.trim(), ",");
+				
+				try
+				{
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					int z = Integer.parseInt(st.nextToken());
+					int heading = Integer.parseInt(st.nextToken());
+					int npc_id = Integer.parseInt(st.nextToken());
+					
+					_commanderSpawns.add(new FortSiegeSpawn(fort.getResidenceId(), x, y, z, heading, npc_id, i));
+				}
+				catch (Exception e)
+				{
+					_log.warn("Error while loading commander(s) for " + fort.getName() + " fort.");
+				}
+			}
+			
+			_commanderSpawnList.put(fort.getResidenceId(), _commanderSpawns);
+			
+			for (int i = 1; i < 4; i++)
+			{
+				String _spawnParams = siegeSettings.getProperty(fort.getName().replace(" ", "") + "Flag" + i, "");
+				if (_spawnParams.isEmpty())
+				{
+					break;
+				}
+				StringTokenizer st = new StringTokenizer(_spawnParams.trim(), ",");
+				
+				try
+				{
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					int z = Integer.parseInt(st.nextToken());
+					int flag_id = Integer.parseInt(st.nextToken());
+					
+					_flagSpawns.add(new CombatFlag(fort.getResidenceId(), x, y, z, 0, flag_id));
+				}
+				catch (Exception e)
+				{
+					_log.warn("Error while loading flag(s) for " + fort.getName() + " fort.");
+				}
+			}
+			_flagList.put(fort.getResidenceId(), _flagSpawns);
+		}
+	}
+	
+	public final FastList<FortSiegeSpawn> getCommanderSpawnList(int _fortId)
+	{
+		if (_commanderSpawnList.containsKey(_fortId))
+		{
+			return _commanderSpawnList.get(_fortId);
+		}
+		return null;
+	}
+	
+	public final FastList<CombatFlag> getFlagList(int _fortId)
+	{
+		if (_flagList.containsKey(_fortId))
+		{
+			return _flagList.get(_fortId);
+		}
+		return null;
+	}
+	
+	public final int getAttackerMaxClans()
+	{
+		return _attackerMaxClans;
+	}
+	
+	public final int getFlagMaxCount()
+	{
+		return _flagMaxCount;
+	}
+	
+	public final boolean canRegisterJustTerritory()
+	{
+		return _justToTerritory;
+	}
+	
+	public final int getSuspiciousMerchantRespawnDelay()
+	{
+		return _suspiciousMerchantRespawnDelay;
+	}
+	
+	public final FortSiege getSiege(L2Object activeObject)
+	{
+		return getSiege(activeObject.getX(), activeObject.getY(), activeObject.getZ());
+	}
+	
+	public final FortSiege getSiege(int x, int y, int z)
+	{
+		for (Fort fort : FortManager.getInstance().getForts())
+		{
+			if (fort.getSiege().checkIfInZone(x, y, z))
+			{
+				return fort.getSiege();
+			}
+		}
+		return null;
+	}
+	
+	public final int getSiegeClanMinLevel()
+	{
+		return _siegeClanMinLevel;
+	}
+	
+	public final int getSiegeLength()
+	{
+		return _siegeLength;
+	}
+	
+	public final int getCountDownLength()
+	{
+		return _countDownLength;
+	}
+	
+	public final List<FortSiege> getSieges()
+	{
+		if (_sieges == null)
+		{
+			_sieges = new FastList<>();
+		}
+		return _sieges;
+	}
+	
+	public final void addSiege(FortSiege fortSiege)
+	{
+		if (_sieges == null)
+		{
+			_sieges = new FastList<>();
+		}
+		_sieges.add(fortSiege);
+	}
+	
+	public boolean isCombat(int itemId)
+	{
+		return (itemId == 9819);
+	}
+	
+	public boolean activateCombatFlag(L2PcInstance player, L2ItemInstance item)
+	{
+		if (!checkIfCanPickup(player))
+		{
+			return false;
+		}
+		
+		Fort fort = FortManager.getInstance().getFort(player);
+		
+		FastList<CombatFlag> fcf = _flagList.get(fort.getResidenceId());
+		for (CombatFlag cf : fcf)
+		{
+			if (cf.getCombatFlagInstance() == item)
+			{
+				cf.activate(player, item);
+			}
+		}
+		return true;
+	}
+	
+	public boolean checkIfCanPickup(L2PcInstance player)
+	{
+		SystemMessage sm;
+		sm = SystemMessage.getSystemMessage(SystemMessageId.THE_FORTRESS_BATTLE_OF_S1_HAS_FINISHED);
+		sm.addItemName(9819);
+		// Cannot own 2 combat flag
+		if (player.isCombatFlagEquipped())
+		{
+			player.sendPacket(sm);
+			return false;
+		}
+		
+		// here check if is siege is in progress
+		// here check if is siege is attacker
+		Fort fort = FortManager.getInstance().getFort(player);
+		
+		if ((fort == null) || (fort.getResidenceId() <= 0))
+		{
+			player.sendPacket(sm);
+			return false;
+		}
+		else if (!fort.getSiege().isInProgress())
+		{
+			player.sendPacket(sm);
+			return false;
+		}
+		else if (fort.getSiege().getAttackerClan(player.getClan()) == null)
+		{
+			player.sendPacket(sm);
+			return false;
+		}
+		return true;
+	}
+	
+	public void dropCombatFlag(L2PcInstance player, int fortId)
+	{
+		Fort fort = FortManager.getInstance().getFortById(fortId);
+		
+		FastList<CombatFlag> fcf = _flagList.get(fort.getResidenceId());
+		
+		for (CombatFlag cf : fcf)
+		{
+			if (cf.getPlayerObjectId() == player.getObjectId())
+			{
+				cf.dropIt();
+				if (fort.getSiege().isInProgress())
+				{
+					cf.spawnMe();
+				}
+			}
+		}
+	}
+	
+	private static class SingletonHolder
+	{
+		protected static final FortSiegeManager _instance = new FortSiegeManager();
+	}
+}
---
+++ L2J_SunriseProject_Core/java/l2r/gameserver/model/actor/instance/L2CustomGatekeeperInstance.java
@@ -0,0 +1,195 @@
+package l2r.gameserver.model.actor.instance;
+
+import l2r.gameserver.enums.InstanceType;
+import l2r.gameserver.enums.ZoneIdType;
+import l2r.gameserver.instancemanager.ZoneManager;
+import l2r.gameserver.model.actor.FakePc;
+import l2r.gameserver.model.actor.L2Character;
+import l2r.gameserver.model.actor.L2Npc;
+import l2r.gameserver.model.actor.templates.L2NpcTemplate;
+import l2r.gameserver.model.zone.L2ZoneType;
+import l2r.gameserver.network.serverpackets.ActionFailed;
+import l2r.gameserver.network.serverpackets.NpcHtmlMessage;
+import gr.sr.configsEngine.configs.impl.CustomNpcsConfigs;
+import gr.sr.datatables.SunriseTable;
+import gr.sr.main.Conditions;
+import gr.sr.securityEngine.SecurityActions;
+import gr.sr.securityEngine.SecurityType;
+
+/**
+ * @author L2jSunrise Team
+ * @Website www.l2jsunrise.com
+ */
+
+public final class L2CustomGatekeeperInstance extends L2Npc
+{
+	/**
+	 * @param objectId
+	 * @param template
+	 */
+	public L2CustomGatekeeperInstance(int objectId, L2NpcTemplate template)
+	{
+		super(objectId, template);
+		setInstanceType(InstanceType.L2CustomGatekeeperInstance);
+		FakePc fpc = getFakePc();
+		if (fpc != null)
+		{
+			setTitle(fpc.title);
+		}
+	}
+	
+	private static int getPlayersInZoneCount(int zoneId)
+	{
+		int playersCount = 0;
+		L2ZoneType zone = ZoneManager.getInstance().getZoneById(zoneId);
+		
+		if (zone != null)
+		{
+			for (L2Character character : zone.getCharactersInside())
+			{
+				if (character.isPlayer())
+				{
+					playersCount++;
+				}
+			}
+		}
+		return playersCount;
+	}
+	
+	@Override
+	public void showChatWindow(L2PcInstance player)
+	{
+		player.sendPacket(ActionFailed.STATIC_PACKET);
+		
+		if (!CustomNpcsConfigs.ENABLE_CUSTOM_GATEKEEPER)
+		{
+			player.sendMessage("Custom gatekeeper is disabled by admin.");
+			return;
+		}
+		
+		if (!CustomNpcsConfigs.ALLOW_TELEPORT_WHILE_COMBAT && player.isInCombat())
+		{
+			player.sendMessage("Cannot use while in combat.");
+			return;
+		}
+		
+		if (!CustomNpcsConfigs.ALLOW_TELEPORT_WITH_KARMA && (player.getKarma() > 0))
+		{
+			player.sendMessage("Cannot use while hava karma.");
+			return;
+		}
+		
+		NpcHtmlMessage html = new NpcHtmlMessage(getObjectId());
+		html.setFile(player.getHtmlPrefix(), "data/html/teleporter/custom/main.htm");
+		if (CustomNpcsConfigs.ENABLE_PLAYERS_COUNT)
+		{
+			switch (CustomNpcsConfigs.ZONE_TYPE_FOR_PLAYERS_COUNT)
+			{
+				case "ChaoticZone":
+					html.replace("%players_count%", String.valueOf(getPlayersInZoneCount(ZoneIdType.ZONE_CHAOTIC.ordinal())));
+					break;
+				case "FlagZone":
+					html.replace("%players_count%", String.valueOf(getPlayersInZoneCount(ZoneIdType.FLAG.ordinal())));
+					break;
+				default:
+					break;
+			}
+		}
+		html.replace("%objectId%", String.valueOf(getObjectId()));
+		player.sendPacket(html);
+	}
+	
+	/**
+	 * Method to manage all player bypasses
+	 * @param player
+	 * @param command
+	 */
+	@Override
+	public void onBypassFeedback(final L2PcInstance player, String command)
+	{
+		final String[] subCommand = command.split("_");
+		
+		// No null pointers
+		if (player == null)
+		{
+			return;
+		}
+		
+		if (player.isEnchanting())
+		{
+			player.sendMessage("Cannot use while Enchanting.");
+			return;
+		}
+		
+		if (player.isAlikeDead())
+		{
+			player.sendMessage("Cannot use while Dead or Fake Death.");
+			return;
+		}
+		
+		// Page navigation, html command how to starts
+		if (command.startsWith("Chat"))
+		{
+			if (subCommand[1].isEmpty() || (subCommand[1] == null))
+			{
+				return;
+			}
+			NpcHtmlMessage html = new NpcHtmlMessage(getObjectId());
+			html.setFile(player.getHtmlPrefix(), "data/html/teleporter/custom/" + subCommand[1]);
+			if (CustomNpcsConfigs.ENABLE_PLAYERS_COUNT)
+			{
+				switch (CustomNpcsConfigs.ZONE_TYPE_FOR_PLAYERS_COUNT)
+				{
+					case "ChaoticZone":
+						html.replace("%players_count%", String.valueOf(getPlayersInZoneCount(ZoneIdType.ZONE_CHAOTIC.ordinal())));
+						break;
+					case "FlagZone":
+						html.replace("%players_count%", String.valueOf(getPlayersInZoneCount(ZoneIdType.FLAG.ordinal())));
+						break;
+					default:
+						break;
+				}
+			}
+			html.replace("%objectId%", String.valueOf(getObjectId()));
+			player.sendPacket(html);
+		}
+		// Teleport
+		else if (command.startsWith("teleportTo"))
+		{
+			int itemIdToGet = CustomNpcsConfigs.TELEPORT_ITEM_ID;
+			int price = CustomNpcsConfigs.TELEPORT_ITEM_AMOUNT;
+			
+			if (!Conditions.checkPlayerItemCount(player, itemIdToGet, price))
+			{
+				return;
+			}
+			
+			if (player.isTransformed())
+			{
+				if ((player.getTransformationId() == 9) || (player.getTransformationId() == 8))
+				{
+					player.untransform();
+				}
+			}
+			
+			if (command.startsWith("teleportToGlobal"))
+			{
+				try
+				{
+					if (player.destroyItemByItemId("Global Teleport", itemIdToGet, price, player, true))
+					{
+						Integer[] c = new Integer[3];
+						c[0] = SunriseTable.getInstance().getCoords(Integer.parseInt(subCommand[1]))[0];
+						c[1] = SunriseTable.getInstance().getCoords(Integer.parseInt(subCommand[1]))[1];
+						c[2] = SunriseTable.getInstance().getCoords(Integer.parseInt(subCommand[1]))[2];
+						player.teleToLocation(c[0], c[1], c[2]);
+					}
+				}
+				catch (Exception e)
+				{
+					SecurityActions.startSecurity(player, SecurityType.CUSTON_GATEKEEPER);
+				}
+			}
+		}
+	}
+}
