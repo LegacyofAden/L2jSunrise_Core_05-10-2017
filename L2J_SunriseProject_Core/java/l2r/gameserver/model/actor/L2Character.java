be622d5ba049f5b75e24d0dd5eb3e12ef4bc011a
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2015-08-05 19:21:30 +0000

    Minor npe in do attack method
    compile and copy paste core.jar

--- L2J_SunriseProject_Core/java/l2r/gameserver/model/actor/L2Character.java
+++ L2J_SunriseProject_Core/java/l2r/gameserver/model/actor/L2Character.java
@@ -866,7 +866,7 @@ public abstract class L2Character extends L2Object implements ISkillsHolder
 		if (getActiveWeaponItem() != null)
 		{
 			L2Weapon wpn = getActiveWeaponItem();
-			if (!wpn.isAttackWeapon() && !isGM())
+			if ((wpn != null) && !wpn.isAttackWeapon() && !isGM())
 			{
 				if (wpn.getItemType() == WeaponType.FISHINGROD)
 				{
