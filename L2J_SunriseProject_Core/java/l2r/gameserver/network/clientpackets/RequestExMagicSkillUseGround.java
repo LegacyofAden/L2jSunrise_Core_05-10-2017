8958eb467a6551c2a4d78ef5a343f71b0f152cae
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2016-06-03 00:32:53 +0000

    Fixed some wrong broadcasts
    Compile and copy paste core.jar

--- L2J_SunriseProject_Core/java/l2r/gameserver/network/clientpackets/RequestExMagicSkillUseGround.java
+++ L2J_SunriseProject_Core/java/l2r/gameserver/network/clientpackets/RequestExMagicSkillUseGround.java
@@ -24,7 +24,6 @@ import l2r.gameserver.model.actor.instance.L2PcInstance;
 import l2r.gameserver.model.skills.L2Skill;
 import l2r.gameserver.network.serverpackets.ActionFailed;
 import l2r.gameserver.network.serverpackets.ValidateLocation;
-import l2r.gameserver.util.Broadcast;
 import l2r.gameserver.util.Util;
 
 /**
@@ -81,7 +80,7 @@ public final class RequestExMagicSkillUseGround extends L2GameClientPacket
 			
 			// normally magicskilluse packet turns char client side but for these skills, it doesn't (even with correct target)
 			activeChar.setHeading(Util.calculateHeadingFrom(activeChar.getX(), activeChar.getY(), _x, _y));
-			Broadcast.toKnownPlayers(activeChar, new ValidateLocation(activeChar));
+			activeChar.broadcastPacket(new ValidateLocation(activeChar));
 			
 			activeChar.useMagic(skill, _ctrlPressed, _shiftPressed);
 		}
