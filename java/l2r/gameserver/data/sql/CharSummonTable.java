d7911fe795cdc4a61d3d75a5edace7abb4245f6f
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2016-09-28 23:39:47 +0000

    Double pet fix
    Compile and copy paste core.jar

--- java/l2r/gameserver/data/sql/CharSummonTable.java
+++ java/l2r/gameserver/data/sql/CharSummonTable.java
@@ -178,8 +178,7 @@ public class CharSummonTable
 	
 	public void restoreServitor(L2PcInstance activeChar)
 	{
-		int skillId = _servitors.get(activeChar.getObjectId());
-		
+		int skillId = _servitors.remove(activeChar.getObjectId());
 		try (Connection con = L2DatabaseFactory.getInstance().getConnection();
 			PreparedStatement ps = con.prepareStatement(LOAD_SUMMON))
 		{
@@ -200,6 +199,7 @@ public class CharSummonTable
 					skill = (L2SkillSummon) SkillData.getInstance().getInfo(skillId, activeChar.getSkillLevel(skillId));
 					if (skill == null)
 					{
+						removeServitor(activeChar);
 						return;
 					}
 					
@@ -240,9 +240,6 @@ public class CharSummonTable
 		{
 			LOGGER.warn(getClass().getSimpleName() + ": Summon cannot be restored: ", e);
 		}
-		
-		// vGodFather we will remove servitor no matter what
-		removeServitor(activeChar);
 	}
 	
 	public void saveSummon(L2ServitorInstance summon)
