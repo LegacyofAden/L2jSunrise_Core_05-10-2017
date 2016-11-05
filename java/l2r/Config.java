ba77a324c5814f889c741f203d5cd5bb0051ad61
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2016-11-05 17:51:43 +0000

    Minor mistakes in packet system
    Compile and copy paste core.jar

--- java/l2r/Config.java
+++ java/l2r/Config.java
@@ -1834,7 +1834,7 @@ public final class Config
 			status_update_packetsDelay = serverPacketsSettings.getInt("StatusUpdatePacketsDelay", 50);
 			effects_packetsDelay = serverPacketsSettings.getInt("EffectsPacketsDelay", 333);
 			stats_update_packetsDelay = serverPacketsSettings.getInt("StatsUpdatePacketsDelay", 100);
-			moveToPawn_packetsDelay = serverPacketsSettings.getInt("MoveToPawnPacketsDelay", 0);
+			moveToPawn_packetsDelay = serverPacketsSettings.getInt("MoveToPawnPacketsDelay", 30);
 			
 			// Load Telnet L2Properties file (if exists)
 			final PropertiesParser telnetSettings = new PropertiesParser(TELNET_FILE);
