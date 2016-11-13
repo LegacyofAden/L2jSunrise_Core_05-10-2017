879c94a51f3e93c8a793624eb7897f1d5ae9dda6
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2016-11-13 09:30:41 +0000

    Fixed some attack issues in territory wars
    Compile and copy paste core.jar

--- java/l2r/gameserver/model/actor/instance/L2PcInstance.java
+++ java/l2r/gameserver/model/actor/instance/L2PcInstance.java
@@ -9222,19 +9222,20 @@ public final class L2PcInstance extends L2Playable
 			
 			if ((target.getActingPlayer() != null) && (getSiegeState() > 0) && isInsideZone(ZoneIdType.SIEGE) && (target.getActingPlayer().getSiegeState() == getSiegeState()) && (target.getActingPlayer() != this) && (target.getActingPlayer().getSiegeSide() == getSiegeSide()))
 			{
-				if (isInSameParty(target.getActingPlayer()) || isInSameChannel(target.getActingPlayer()) || isInSameClan(target.getActingPlayer()) || isInSameAlly(target.getActingPlayer()))
+				// vGodFather: no need this
+				// if (isInSameParty(target.getActingPlayer()) || isInSameChannel(target.getActingPlayer()) || isInSameClan(target.getActingPlayer()) || isInSameAlly(target.getActingPlayer()))
+				// {
+				if (TerritoryWarManager.getInstance().isTWInProgress())
 				{
-					if (TerritoryWarManager.getInstance().isTWInProgress())
-					{
-						sendPacket(SystemMessageId.YOU_CANNOT_ATTACK_A_MEMBER_OF_THE_SAME_TERRITORY);
-					}
-					else
-					{
-						sendPacket(SystemMessageId.FORCED_ATTACK_IS_IMPOSSIBLE_AGAINST_SIEGE_SIDE_TEMPORARY_ALLIED_MEMBERS);
-					}
-					sendPacket(ActionFailed.STATIC_PACKET);
-					return false;
+					sendPacket(SystemMessageId.YOU_CANNOT_ATTACK_A_MEMBER_OF_THE_SAME_TERRITORY);
 				}
+				else
+				{
+					sendPacket(SystemMessageId.FORCED_ATTACK_IS_IMPOSSIBLE_AGAINST_SIEGE_SIDE_TEMPORARY_ALLIED_MEMBERS);
+				}
+				sendPacket(ActionFailed.STATIC_PACKET);
+				return false;
+				// }
 			}
 			
 			// Check if the target is attackable
