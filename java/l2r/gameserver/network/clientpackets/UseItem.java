aac02f547fb355cf0959863c6cd812eb58966a40
vGodFather <vGodFather@d2703588-c4d4-4869-b5f7-84ce14587d84>
2017-05-14 02:46:46 +0000

    Fixed hot springs lure can't be equipped
    Compile and copy paste core.jar

--- java/l2r/gameserver/network/clientpackets/UseItem.java
+++ java/l2r/gameserver/network/clientpackets/UseItem.java
@@ -361,7 +361,7 @@ public final class UseItem extends L2GameClientPacket
 			}
 			
 			final L2Weapon weaponItem = activeChar.getActiveWeaponItem();
-			if (((weaponItem != null) && (weaponItem.getItemType() == WeaponType.FISHINGROD)) && (((_itemId >= 6519) && (_itemId <= 6527)) || ((_itemId >= 7610) && (_itemId <= 7613)) || ((_itemId >= 7807) && (_itemId <= 7809)) || ((_itemId >= 8484) && (_itemId <= 8486)) || ((_itemId >= 8505) && (_itemId <= 8513))))
+			if (((weaponItem != null) && (weaponItem.getItemType() == WeaponType.FISHINGROD)) && (((_itemId >= 6519) && (_itemId <= 6527)) || ((_itemId >= 7610) && (_itemId <= 7613)) || ((_itemId >= 7807) && (_itemId <= 7809)) || ((_itemId >= 8484) && (_itemId <= 8486)) || ((_itemId >= 8505) && (_itemId <= 8513)) || (_itemId == 8548)))
 			{
 				activeChar.getInventory().setPaperdollItem(Inventory.PAPERDOLL_LHAND, item);
 				activeChar.broadcastUserInfo();
